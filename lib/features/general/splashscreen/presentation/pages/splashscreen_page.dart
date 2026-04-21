c95a4f7f2f51c6c21f9c18cfc033d839178c007a
StarSkyGeminid <akungithubku@gmail.com>
2026-04-21 18:07:23 +0700

    Refactor SplashScreenPage to include BlocListener for location state changes

--- lib/features/general/splashscreen/presentation/pages/splashscreen_page.dart
+++ lib/features/general/splashscreen/presentation/pages/splashscreen_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:reinmkg/core/resources/images.dart';
+import 'package:reinmkg/features/general/location/presentation/cubit/location_cubit.dart';
 
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({super.key});
@@ -9,19 +12,25 @@ class SplashScreenPage extends StatefulWidget {
 }
 
 class SplashScreenPageState extends State<SplashScreenPage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ColoredBox(
-        color: Theme.of(context).canvasColor,
-        child: Center(
-          child: CircleAvatar(
-            backgroundColor: Theme.of(context).hintColor,
-            radius: 96,
+    return BlocListener<LocationCubit, LocationState>(
+      listener: (context, state) {
+        if (state is LocationLoaded) {
+          context.go('/dashboard');
+        }
+      },
+      child: Scaffold(
+        body: ColoredBox(
+          color: Theme.of(context).canvasColor,
+          child: Center(
             child: CircleAvatar(
-              backgroundImage: AssetImage(Images.icLogo),
+              backgroundColor: Theme.of(context).hintColor,
               radius: 96,
+              child: CircleAvatar(
+                backgroundImage: AssetImage(Images.icLogo),
+                radius: 96,
+              ),
             ),
           ),
         ),

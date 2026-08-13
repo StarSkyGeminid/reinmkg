742ae91d05ff35a30e726c8779451435e8443725
StarSkyGeminid <akungithubku@gmail.com>
2026-08-13 18:09:27 +0700

    Add celestial details: solar noon, dawn/dusk, golden hour, moon distance, timezone suffix

--- lib/features/celestial/data/models/celestial_object_model.dart
+++ lib/features/celestial/data/models/celestial_object_model.dart
@@ -11,9 +11,23 @@ class CelestialObjectModel extends CelestialObjectEntity {
     super.angle,
     super.riseTime,
     super.setTime,
+    super.solarNoon,
+    super.dawn,
+    super.dusk,
+    super.goldenHour,
+    super.waxing,
+    super.alwaysUp,
+    super.alwaysDown,
   });
 
   factory CelestialObjectModel.fromJson(Map<String, dynamic> json) {
+    DateTime? parseDate(String key) {
+      final v = json[key];
+      if (v == null) return null;
+      if (v is DateTime) return v;
+      return DateTime.parse(v as String);
+    }
+
     var riseTime = json['rise'] != null
         ? json['rise'] is DateTime
               ? json['rise']
@@ -43,6 +57,13 @@ class CelestialObjectModel extends CelestialObjectEntity {
       angle: (json['angle'] as num?)?.toDouble(),
       riseTime: riseTime,
       setTime: setTime,
+      solarNoon: parseDate('solarNoon'),
+      dawn: parseDate('dawn'),
+      dusk: parseDate('dusk'),
+      goldenHour: parseDate('goldenHour'),
+      waxing: json['waxing'] as bool?,
+      alwaysUp: json['alwaysUp'] as bool?,
+      alwaysDown: json['alwaysDown'] as bool?,
     );
   }
 }
